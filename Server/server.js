1227d2eaf4ee583545e8dee78840daf1695db4ea
Presley Maphosa <presleymaphosa@sgwimbi-lap.higherlifefoundation.org>
2019-07-25 12:33:14 +0200

    Adding to heroku

--- Server/server.js
+++ Server/server.js
@@ -42,7 +42,7 @@ app.use("/api/upload", fileUploadRoute);
 
 //********** ROUTES END
 
-if (process.env.MODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname + "/../Client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname + "/../Client/build/index.html"));
