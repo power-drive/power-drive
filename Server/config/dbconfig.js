e136bd3a6224aef6503ffad76baf9caa18356f49
Presley Maphosa <presleymaphosa@sgwimbi-lap.higherlifefoundation.org>
2019-07-25 12:17:56 +0200

    Adding to heroku

--- Server/config/dbconfig.js
+++ Server/config/dbconfig.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 dbconnection = () => {
-  mongoose.connect(config.get("MONGOURI") || config.get("mongoURI"));
+  mongoose.connect(process.env.MONGOURI || config.get("mongoURI"));
   let db = mongoose.connection;
 
   //Check for connection
