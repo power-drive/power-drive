bdf5928c39c6844897fe6149585ce8b26c12a1bc
Presley Maphosa <presleymaphosa@Presleys-MacBook-Pro.local>
2019-07-27 07:04:35 +0200

    fg fff

--- Server/routes/file_uploadRoute.js
+++ Server/routes/file_uploadRoute.js
@@ -9,7 +9,7 @@ uploadRoute.use(bodyParser.json());
 
 var storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "/uploads/");
   },
   filename: function(req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
